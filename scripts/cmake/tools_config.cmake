4b83e1deb2ee238f93f82c37a27028f65dfb5d0b
Manel J. <manel.jimeno@gmail.com>
2024-12-06 16:07:22 +0100

    Add a custom target to build menus and the necessary resources

--- scripts/cmake/tools_config.cmake
+++ scripts/cmake/tools_config.cmake
@@ -19,3 +19,16 @@ function(generate_json_sources target json_dir generated_dir)
 
     add_dependencies(${target}_gen_tables db_api_generator)
 endfunction()
+
+# Function to generate .cpp files from .json files
+function(generate_qml_sources target menu_dir generated_dir)
+    message(STATUS "Loading JSON files from ${json_dir} the output directory is ${generated_dir}")
+
+    add_custom_target(
+        ${target}_gen_menus
+        COMMAND menu_generator --source-dir ${menu_dir} --output ${generated_dir}
+        COMMENT "Generating QML code from ${menu_dir} to ${generated_dir}"
+        VERBATIM)
+
+    add_dependencies(${target}_gen_tables db_api_generator)
+endfunction()
